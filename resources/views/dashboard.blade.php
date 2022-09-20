a8b11af129ed048616df44d2f598a3401e3324d3
Vinicius Peres <vperes505@gmail.com>
2022-09-20 07:08:40 -0300

    view dashboard feita

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container ">
+    <div class="container bg-light">
         <div class="row p-3 ">
             <div class="col-md-4">
                 <div class="card">
-                    <div class="card-body bg-light">
+                    <div class="card-body ">
                         <a href="#" class="btn ">Minha conta</a> <br>
                         <a href="#" class="btn ">Meus pedidos</a>
                         <hr>
@@ -16,8 +16,7 @@
 
             <div class="col-md-8">
                 <div>
-                    <i></i>
-                    <h4 class="h3">Minha conta</h4>
+                    <h4 class="h3"><i class="fa fa-user"></i> Minha conta</h4>
                     <hr>
                     <h4>Informações de acesso</h4>    
 
@@ -29,8 +28,7 @@
                 </div>
                       
                 <div class="d-flex justify-content-start mt-4">
-                    <i></i>
-                    <h4 class="mt-1">Endereços Cadastrados</h4> 
+                    <h4 class="mt-1"> <i class="fas fa-map-marker-alt"></i> Endereços Cadastrados</h4> 
                     <a href="#" class="btn btn-success mx-3">Gerenciar Endereços</a>
                 </div>
 
@@ -62,30 +60,34 @@
                     </div>
                 </div>
 
-                <div class="card mt-4">
-                    <div class="card-body">
-                        <table class="table">
-                            <thead class="list-inline">
-                                <th>#</th>
-                                <th>Data</th>
-                                <th>Pagamento</th>
-                                <th>Valor total</th>
-                                <th>Status</th>
-                                <th></th>
-                            </thead>
-        
-                            <tbody>
-                                <td> 817263 </td>
-                                <td> 02/07/2022 </td>
-                                <td> PIX </td>
-                                <td> R$ 99,90 </td>
-                                <td> Entregue </td>
-                                <td> <a href="#" class="btn btn-success">Ver pedido</a> </td>
-                            </tbody>
-                        </table>
+                <div class="col-md-12 mt-4">
+                    <h4><i class="fas fa-ticket-alt"> </i> Meus Pedidos </h4>
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table">
+                                <thead class="list-inline">
+                                    <th>#</th>
+                                    <th>Data</th>
+                                    <th>Pagamento</th>
+                                    <th>Valor total</th>
+                                    <th>Status</th>
+                                    <th></th>
+                                </thead>
+            
+                                <tbody>
+                                    <td> 817263 </td>
+                                    <td> 02/07/2022 </td>
+                                    <td> PIX </td>
+                                    <td> R$ 99,90 </td>
+                                    <td> Entregue </td>
+                                    <td> <a href="#" class="btn btn-success">Ver pedido</a> </td>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 
+
             </div>
         </div>
     </div>
